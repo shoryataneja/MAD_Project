babf28a3d1f9ef767fd333b2db1261772fc29d1a
shoryataneja <shoryataneja5@gmail.com>
2025-11-25 00:39:01 +0530

    added the app version

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,14 +1,25 @@
 import React from "react";
 import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { Icon } from "react-native-paper";
 
 export default function AboutScreen() {
   return (
     <ScrollView contentContainerStyle={styles.container}>
       
-      {/* App Title */}
-      <Text style={styles.title}>About</Text>
+      {/* App Logo */}
+      <View style={styles.logoCircle}>
+        <Icon source="coffee" size={40} color="#6F4E37" />
+      </View>
 
-      {/* Content will be added in next commits */}
+      {/* App Name */}
+      <Text style={styles.title}>Campus Chai Tracker</Text>
+      <Text style={styles.tagline}>Track your daily chai & coffee habits ☕📊</Text>
+
+      {/* --- App Version Card --- */}
+      <View style={styles.card}>
+        <Text style={styles.cardTitle}>App Version</Text>
+        <Text style={styles.cardText}>Campus Chai Tracker v1.0.0</Text>
+      </View>
 
     </ScrollView>
   );
@@ -28,5 +39,26 @@ const styles = StyleSheet.create({
     color: "#6F4E37",
     marginTop: 40,
     marginBottom: 20,
+  },
+
+  card: {
+    backgroundColor: "#FFEEDB",
+    width: "100%",
+    padding: 18,
+    borderRadius: 15,
+    marginBottom: 20,
+  },
+
+  cardTitle: {
+    fontSize: 17,
+    fontWeight: "600",
+    color: "#6F4E37",
+    marginTop: 8 ,
+  },
+
+  cardText: {
+    fontSize: 15,
+    color: "#8B6B4A",
+    lineHeight: 20,
   },
 });
